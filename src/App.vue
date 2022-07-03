9015cabfb1b8fadb5805206585d7c1d518af95fc
wl180 <wlwdsqa@163.com>
2022-07-03 15:21:42 +0800

    封装了icon组件

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,14 @@
 <template>
   <div>
     <div>ok</div>
+    <my-icon icon="dianzan"></my-icon>
+    <MyIcon icon="wode"></MyIcon>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
-export default {
-  data() {
-    return {};
-  },
-};
+export default {};
 </script>
 
 <style scoped lang="less"></style>
---
+++ src/components/MyIcon.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <i :class="['toutiao', `toutiao-${icon}`]"></i>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    icon: {
+      type: String,
+    },
+  },
+};
+</script>
+
+<style></style>
